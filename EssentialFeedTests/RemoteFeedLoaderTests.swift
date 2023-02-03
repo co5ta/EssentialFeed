2720c0ef6c8ca59de554faa0dc9ef686931e90ca
Costa Monzili <costa.monzili@adevinta.com>
2023-02-03 23:44:56 +0100

    add RemoteFeedLoader components

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,60 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Costa Monzili on 03/02/2023.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    let url: URL
+    let client: HTTPClient
+
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+
+        XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load()
+
+        XCTAssertEqual(client.requestedURL, url)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(url: URL = URL(string: "https://any-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+}
