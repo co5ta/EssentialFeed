c276b3c8b0d138416c845ea55730a572dae467e3
Costa Monzili <costa.monzili@adevinta.com>
2023-04-10 12:33:04 +0200

    move `LocalFeedLoader` and `FeedStore` collaborator to its own file in production

---
+++ EssentialFeed/FeedCache/LocaleFeedLoader.swift
@@ -0,0 +1,45 @@
+//
+//  LocaleFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Costa Monzili on 10/04/2023.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
+
+public final class LocalFeedLoader {
+    private let store: FeedStore
+    private let currentDate: () -> Date
+
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
+
+            if let error = error {
+                completion(error)
+            } else {
+                self.cache(items, with: completion)
+            }
+        }
+    }
+
+    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+        store.insert(items, timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        }
+    }
+}
