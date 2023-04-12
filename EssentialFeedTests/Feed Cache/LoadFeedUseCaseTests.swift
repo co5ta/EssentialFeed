67674dc5d05c870af1a7570455e583285e1052aa
Costa Monzili <costa.monzili@adevinta.com>
2023-04-12 13:15:57 +0200

    replace load command completion to return a result type rather than an optional error

--- EssentialFeedTests/Feed Cache/LoadFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedUseCaseTests.swift
@@ -30,8 +30,13 @@ final class LoadFeedUseCaseTests: XCTestCase {
         let exp = expectation(description: "wait for load completion")
 
         var receivedError: Error?
-        sut.load { error in
-            receivedError = error
+        sut.load { result in
+            switch result {
+            case let .failure(error):
+                receivedError = error
+            default:
+                XCTFail("expected failure result, got \(result) instead")
+            }
             exp.fulfill()
         }
 
