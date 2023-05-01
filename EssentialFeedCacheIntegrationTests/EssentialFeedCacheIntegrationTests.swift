87ad841443065d567a9fc6f16a78a0b1b89e4ac8
Costa Monzili <costa.monzili@adevinta.com>
2023-05-01 13:05:50 +0200

    add `EssentialFeedCacheIntegrationTests` target to separate the potentially-slower cache integration tests from the fast unit tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Costa Monzili on 01/05/2023.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
