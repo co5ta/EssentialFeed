d028b63473522ccf7f21051748d7aaa5c771145e
Costa Monzili <costa.monzili@adevinta.com>
2023-06-19 19:14:48 +0200

    feed image view retry button is visible on image url load error

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -147,6 +147,27 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(view0?.renderedImage, imageData0, "expected no image state change for first view once second image loading completes successfully")
         XCTAssertEqual(view1?.renderedImage, imageData1, "expected image for second view once second image loading completes successfully")
     }
+
+    func test_feedImageViewRetryButton_isVisibleOnImageURLLoadError() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [makeImage(), makeImage()])
+
+        let view0 = sut.simulateFeedImageViewVisible(at: 0)
+        let view1 = sut.simulateFeedImageViewVisible(at: 1)
+        XCTAssertEqual(view0?.isShowingRetryAction, false, "expected no retry action for first view while loading first image")
+        XCTAssertEqual(view1?.isShowingRetryAction, false, "expected no retry action for second view while loading second image")
+
+        let imageData = UIImage.make(withColor: .red).pngData()!
+        loader.completeImageLoading(with: imageData, at: 0)
+        XCTAssertEqual(view0?.isShowingRetryAction, false, "expected no retry action for first view once first image loading completes successfully")
+        XCTAssertEqual(view1?.isShowingRetryAction, false, "expected no retry action state change for second view once first image loading completes successfully")
+
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(view0?.isShowingRetryAction, false, "expected no retry action state change for first view once second image loading completes successfully")
+        XCTAssertEqual(view1?.isShowingRetryAction, true, "expected retry action for second view once second image loading completes with error")
+    }
 }
 
 // MARK: - Helpers
@@ -284,6 +305,10 @@ private extension FeedImageCell {
 
     var isShowingImageLoadingIndicator: Bool {
         return feedImageContainer.isShimmering
+    }
+
+    var isShowingRetryAction: Bool {
+        return !feedImageButton.isHidden
     }
 
     var locationText: String? {
