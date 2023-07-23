3260c0c9a46a1bde943ab11ac72f3d01dca3101e
Costa Monzili <costa.monzili@adevinta.com>
2023-07-23 17:52:04 +0200

    move ErrorView to separate file

---
+++ EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Costa Monzili on 23/07/2023.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
