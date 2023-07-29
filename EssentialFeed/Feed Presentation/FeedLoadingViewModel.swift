08c70d2821335770224e40a68da4e6b45de194f5
Costa Monzili <costa.monzili@adevinta.com>
2023-07-29 21:24:07 +0200

    move FeedLoadingViewModel to separate file

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Costa Monzili on 29/07/2023.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
