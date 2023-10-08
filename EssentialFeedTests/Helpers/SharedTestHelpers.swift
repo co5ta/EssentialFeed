818f8854f7e1aebe7eac0fe684199a329a2346dd
Costa Monzili <costa.monzili@adevinta.com>
2023-10-08 22:07:14 +0200

    Move test helpers to shared scope

--- EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -22,3 +22,14 @@ var anyData: Data {
 var emptyData: Data {
     Data()
 }
+
+func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+    let json = ["items": items]
+    return try! JSONSerialization.data(withJSONObject: json)
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL, statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
