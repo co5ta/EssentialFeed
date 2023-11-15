24f05b33d30d6958fc50d0bf174764ae8f6bb370
Costa Monzili <costa.monzili@adevinta.com>
2023-11-15 11:15:11 +0100

    Fix FeedSnapshotTests with missing selection closure

---
+++ EssentialFeed/FeedAPI/FeedEndPoint.swift
@@ -0,0 +1,8 @@
+//
+//  FeedEndPoint.swift
+//  EssentialFeed
+//
+//  Created by Costa Monzili on 15/11/2023.
+//
+
+import Foundation
---
+++ EssentialFeed/Image Comments API/ImageCommentsEndPoint.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCommentsEndPoint.swift
+//  EssentialFeed
+//
+//  Created by Costa Monzili on 15/11/2023.
+//
+
+import Foundation
